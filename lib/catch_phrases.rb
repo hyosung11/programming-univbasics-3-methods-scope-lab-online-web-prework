31350289fc9996c313acac2c6ffc42bfebfa6820
HyoSung Bidol-Lee <hyosung11@me.com>
2019-06-07 21:43:17 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,7 @@
 def mario
-  
   puts phrase = "It's-a me, Mario!"
 end
 
 def toadstool
-  puts status status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 end
